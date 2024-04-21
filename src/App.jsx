9750bcae345c017e5f356897d31ca4e83c29564a
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-04-21 11:06:07 -0500

    improved menu component

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ export default function App() {
         <div className="min-h-dvh bg-orange-50">
             <Menu className="fixed inset-x-0 top-0 text-2xl">
                 <Menu.Button className="px-3 py-2 fixed z-20">
-                    <IoMenu />
+                    {open => !open ? <IoMenu /> : <IoMenu className="text-white"/>}
                 </Menu.Button>
                 <Menu.Dropdown className="fixed inset-x-0 top-0 text-center bg-sky-700 grid gap-2 py-3 pb-4 text-white shadow-md">
                     <Menu.Item>Lista de compras</Menu.Item>
                     <Menu.Item>Recetas</Menu.Item>
-                    <button className="fixed top-0 right-0 px-3 py-2"><IoClose /></button>
+                    <Menu.Button className="fixed top-0 right-0 px-3 py-2">
+                        {open => <IoClose />}
+                    </Menu.Button>           
                 </Menu.Dropdown>
             </Menu>
             <Outlet />
--- src/components/Menu/MenuButton.jsx
+++ src/components/Menu/MenuButton.jsx
@@ -2,20 +2,24 @@ import { useContext } from "react"
 import { MenuContext } from "./Menu"
 
 
-export default function MenuButton({children, onClick = () => {}, ...rest}) {
-    const {toggleOpen} = useContext(MenuContext)
-    
-    function handleClick() {
-        toggleOpen()
-        onClick()
-    }
+export default function MenuButton({
+        children = () => {}, 
+        onClick = () => {}, 
+        ...rest
+    }) {
+        const {open, toggleOpen} = useContext(MenuContext)
+        
+        function handleClick() {
+            toggleOpen()
+            onClick()
+        }
 
-    return (
-        <button 
-            onClick={handleClick} 
-            {...rest}
-        >
-            {children}
-        </button>
-    )
+        return (
+            <button 
+                onClick={handleClick} 
+                {...rest}
+            >
+                {children(open)}
+            </button>
+        )
 }
