d523c0f5767ebf85905782289d781b212fa94ca4
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-04-21 10:28:01 -0500

    add basic markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { Outlet } from "react-router-dom"
+import Menu from "./components/Menu/Index"
+import { IoMenu } from "react-icons/io5"
+
 
 export default function App() {
 
     return (
         <div className="min-h-dvh bg-orange-50">
+            <Menu>
+                <Menu.Button>
+                    <IoMenu />
+                </Menu.Button>
+                <Menu.Dropdown>
+                    <Menu.Item>Lista de compras</Menu.Item>
+                    <Menu.Item>Recetas</Menu.Item>
+                </Menu.Dropdown>
+            </Menu>
             <Outlet />
         </div>
     )
