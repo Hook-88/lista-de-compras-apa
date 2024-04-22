c0ece6ae6f6740c66ade9cc03fde8fd7b065efc5
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-04-21 19:36:10 -0500

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- src/components/AddItemToFirebase.jsx
+++ src/components/AddItemToFirebase.jsx
@@ -1,22 +1,32 @@
 import Form from "./Form"
-
 import { onSnapshot, doc, addDoc } from "firebase/firestore"
 import { db, recipesCollection } from "../firebase"
+import { useState } from "react"
 
 
 
-export default function AddItemToFireBase() {
+export default function AddItemToFireBase({addFunction}) {
+    const [formData, setFormData] = useState("")
 
-    // TODO add function to add ingredient
+    function handleChange(event) {
+        setFormData(event.target.value)
+    }
+
+    function handleSubmit() {
+        addFunction(formData)
+        setFormData("")
+    }
 
     return (
-        <Form className="flex">
+        <Form className="flex" onSubmit={handleSubmit}>
             <input 
                 type="text" 
                 className="rounded px-2 pt-2 flex-1 stretch" 
                 placeholder="Nombre ingrediente"
                 required
                 autoFocus
+                value={formData}
+                onChange={handleChange}
             />
             <button className="px-3 py-2 bg-sky-700 text-white rounded-r">add</button>
         </Form>
--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -1,10 +1,13 @@
 import { Link, useParams } from "react-router-dom"
 import { onSnapshot, doc, updateDoc, getDoc } from "firebase/firestore"
 import { db, recipesCollection } from "../firebase"
-import { useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react"
+import { nanoid } from "nanoid"
 import Card from "../components/Card"
 import Listitem from "../components/Listitem"
 import AddItemToFireBase from "../components/AddItemToFirebase"
+
+const RecipeContext = createContext()
 
 export default function RecipePage() {
     const { id } = useParams()
@@ -24,24 +27,40 @@ export default function RecipePage() {
         return unsub
     },[])
 
+    async function addIngredient(value) {
+        const docRef = doc(db, "recipes", id)
+        const docSnap = await getDoc(docRef)
+        const ingredientObj = {
+            name: value,
+            id: nanoid()
+        }
+        const newIngredientsArray = [...docSnap.data().ingredients, ingredientObj]
+
+        await updateDoc(docRef, { ingredients: newIngredientsArray})   
+    }
+
     return (
         recipe ?
+            <RecipeContext.Provider value={{addIngredient}}>
             <div className="min-h-dvh bg-orange-50">
                 <header className="text-center text-2xl border-b border-slate-300 py-2 mb-2">
                     <h1>{recipe.name}</h1>
                 </header>
                 <main 
                     className="px-2"
-                >
+                    >
                     <Card className="pt-1">
                         <ul>
                             <Listitem>kaas</Listitem>
                             <Listitem>ham</Listitem>
                             <Listitem>sla</Listitem>
-                            <Listitem className="p-0"><AddItemToFireBase /></Listitem>
+                            <Listitem className="p-0"><AddItemToFireBase addFunction={addIngredient}/></Listitem>
                         </ul>
                     </Card>
                 </main>
-            </div> : <h1>Loading...</h1>
+            </div> 
+            </RecipeContext.Provider> : <h1>Loading...</h1>
     )
 }
+
+export { RecipeContext }
